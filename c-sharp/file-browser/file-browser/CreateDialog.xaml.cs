39e8e0f1ff3495515e872e364ab86962c128a85b
FilipSzweda <filip.szweda1@gmail.com>
2022-05-13 22:59:10 +0200

    Finish file browser

---
+++ c-sharp/file-browser/file-browser/CreateDialog.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace file_browser {
+    public partial class CreateDialog : Window {
+        public bool correct;
+        public string path;
+        public CreateDialog(string folderPath) {
+            InitializeComponent();
+            path = folderPath;
+            correct = false;
+        }
+
+        private void OKClick(object sender, RoutedEventArgs e) {
+            bool isFile = (bool)radioFile.IsChecked;
+            bool isDirectory = (bool)radioDirectory.IsChecked;
+            if (isFile && !Regex.IsMatch(textName.Text, "^[a-zA-Z0-9_~-]{1,8}(.txt|.php|.html)$")) {
+                MessageBox.Show("Wrong name!", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else {
+                string name = textName.Text;
+                path = path + "\\" + name;
+                FileAttributes attributes = FileAttributes.Normal;
+                if ((bool)checkA.IsChecked) {
+                    attributes |= FileAttributes.Archive;
+                }
+                    
+                if ((bool)checkR.IsChecked) {
+                    attributes |= FileAttributes.ReadOnly;
+                }
+                    
+                if ((bool)checkH.IsChecked) {
+                    attributes |= FileAttributes.Hidden;
+                }
+                    
+                if ((bool)checkS.IsChecked) {
+                    attributes |= FileAttributes.System;
+                }
+                    
+                if (isFile) {
+                    File.Create(path);
+                }
+                else if (isDirectory) {
+                    Directory.CreateDirectory(path);
+                }
+                File.SetAttributes(path, attributes);
+                Close();
+                correct = true;
+            }
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
--- c-sharp/file-browser/file-browser/MainWindow.xaml.cs
+++ c-sharp/file-browser/file-browser/MainWindow.xaml.cs
@@ -1,82 +1,162 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.IO;
 
-namespace file_browser
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace file_browser {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
 
-        TreeViewItem prepareDirectoryItem(DirectoryInfo directory)
-        {
-            var directoryName = directory.Name;
-            var directoryPath = directory.FullName;
-            var directoryItem = new TreeViewItem { Header = directoryName, Tag = directoryPath };
+        public TreeViewItem NewTreeViewItem(string path) {
+            FileInfo file = new FileInfo(path);
+            FileAttributes attr = File.GetAttributes(path);
+            string header_1;
+            var item = new TreeViewItem {
+                Header = file.Name,
+                Tag = path
+            };
+            item.ContextMenu = new System.Windows.Controls.ContextMenu();
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory) {
+                header_1 = "Create";
+            }
+            else {
+                header_1 = "Open";
+            }
+            var menuItem_1 = new System.Windows.Controls.MenuItem { Header = header_1 };
+            var menuItem_2 = new System.Windows.Controls.MenuItem { Header = "Delete" };
+            item.ContextMenu.Items.Add(menuItem_1);
+            item.ContextMenu.Items.Add(menuItem_2);
+            menuItem_2.Click += new RoutedEventHandler(ContextDeleteClick);
+            if (header_1 == "Open") {
+                menuItem_1.Click += new RoutedEventHandler(ContextOpenClick);
+            }
+            if (header_1 == "Create") {
+                menuItem_1.Click += new RoutedEventHandler(ContextCreateClick);
+            }
+            item.Selected += new RoutedEventHandler(StatusBarDOSAttributes);
+            return item;
+        }
+
+        TreeViewItem PrepareDirectoryItem(DirectoryInfo directory) {
+            var directoryItem = NewTreeViewItem(directory.FullName);
             FileInfo[] subFiles = directory.GetFiles();
-            foreach (FileInfo subFile in subFiles)
-            {
-                var itemName = subFile.Name;
-                var itemPath = subFile.FullName;
-                var item = new TreeViewItem { Header = itemName, Tag = itemPath };
+            foreach (FileInfo subFile in subFiles) {
+                var item = NewTreeViewItem(subFile.FullName);
                 directoryItem.Items.Add(item);
             }
             DirectoryInfo[] subDirectories = directory.GetDirectories();
-            foreach (DirectoryInfo subDirectory in subDirectories)
-            {
-                var item = prepareDirectoryItem(subDirectory);
+            foreach (DirectoryInfo subDirectory in subDirectories) {
+                var item = PrepareDirectoryItem(subDirectory);
                 directoryItem.Items.Add(item);
             }
             return directoryItem;
         }
 
-        private void OpenClick(object sender, RoutedEventArgs e)
-        {
+        private void OpenClick(object sender, RoutedEventArgs e) {
             var folderBrowserDialog = new FolderBrowserDialog() { Description = "Select folder to open" };
             folderBrowserDialog.ShowDialog();
             var rootPath = folderBrowserDialog.SelectedPath;
             var rootDirectory = new DirectoryInfo(rootPath);
-            var rootName = rootDirectory.Name;
-            var root = new TreeViewItem { Header = rootName, Tag = rootPath };
+            var root = NewTreeViewItem(rootPath);
             FileInfo[] subFiles = rootDirectory.GetFiles();
             foreach (FileInfo subFile in subFiles) {
-                var itemName = subFile.Name;
-                var itemPath = subFile.FullName;
-                var item = new TreeViewItem { Header = itemName, Tag = itemPath };
+                var item = NewTreeViewItem(subFile.FullName);
                 root.Items.Add(item);
             }
             DirectoryInfo[] subDirectories = rootDirectory.GetDirectories();
-            foreach (DirectoryInfo subDirectory in subDirectories)
-            {
-                var item = prepareDirectoryItem(subDirectory); 
+            foreach (DirectoryInfo subDirectory in subDirectories) {
+                var item = PrepareDirectoryItem(subDirectory);
                 root.Items.Add(item);
             }
-            System.Windows.Controls.TreeView treeView = this.FindName("MyTreeView") as System.Windows.Controls.TreeView;
-            treeView.Items.Add(root);
+            myTreeView.Items.Add(root);
         }
 
-        private void ExitClick(object sender, RoutedEventArgs e)
-        {
-            System.Windows.Application.Current.Shutdown();
+        private void ExitClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void DeleteDirectory(string path) {
+            foreach (string directory in Directory.GetDirectories(path)) {
+                DeleteDirectory(directory);
+            }
+            foreach (string file in Directory.GetFiles(path)) {
+                File.Delete(file);
+            }
+            Directory.Delete(path);
+        }
+
+        private void ContextDeleteClick(object sender, System.EventArgs e) {
+            TreeViewItem selectedItem = myTreeView.SelectedItem as TreeViewItem;
+            string path = selectedItem.Tag as string;
+            FileAttributes attributes = File.GetAttributes(path);
+            File.SetAttributes(path, attributes & ~FileAttributes.ReadOnly);
+            TreeViewItem parent = (TreeViewItem)selectedItem.Parent;
+            parent.Items.Remove(selectedItem);
+            if ((attributes & FileAttributes.Directory) != FileAttributes.Directory) {
+                File.Delete(path);
+            }
+            else {
+                DeleteDirectory(path);
+            } 
+        }
+
+        private void StatusBarDOSAttributes(object sender, System.EventArgs e) {
+            TreeViewItem item = myTreeView.SelectedItem as TreeViewItem;
+            string file = item.Tag as string;
+            string attributes = "";
+
+            if ((File.GetAttributes(file) & FileAttributes.ReadOnly) == FileAttributes.ReadOnly) {
+                attributes += "r";
+            }  
+            else {
+                attributes += "-";
+            }
+
+            if ((File.GetAttributes(file) & FileAttributes.Archive) == FileAttributes.Archive) {
+                attributes += "a";
+            }  
+            else {
+                attributes += "-";
+            }
+                
+
+            if ((File.GetAttributes(file) & FileAttributes.System) == FileAttributes.System) {
+                attributes += "s";
+            } 
+            else {
+                attributes += "-";
+            }
+                
+
+            if ((File.GetAttributes(file) & FileAttributes.Hidden) == FileAttributes.Hidden) {
+                attributes += "h";
+            } 
+            else {
+                attributes += "-";
+            }
+                
+            currentStatus.Text = attributes;
+        }
+
+        private void ContextOpenClick(object sender, System.EventArgs e) {
+            TreeViewItem item = myTreeView.SelectedItem as TreeViewItem;
+            string path = item.Tag as string;
+            StreamReader stream = new StreamReader(path);
+            scrollViewer.Content = new TextBlock() { Text = stream.ReadToEnd() };
+        }
+
+        private void ContextCreateClick(object sender, System.EventArgs e) {
+            TreeViewItem folder = myTreeView.SelectedItem as TreeViewItem;
+            string path = folder.Tag as string;
+            CreateDialog createDialog = new CreateDialog(path);
+            createDialog.ShowDialog();
+            TreeViewItem newItem;
+            if (createDialog.correct) {
+                newItem = NewTreeViewItem(createDialog.path);
+                folder.Items.Add(newItem);
+            }
         }
     }
 }
